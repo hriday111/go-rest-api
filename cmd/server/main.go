1e522433c5e99e40ec3fac1f88a7b2ead47147b4
friday <hbarot@localhost.localdomain>
2025-04-28 16:46:49 +0200

    added postgressql connection, with password hashing

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,20 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"encoding/json"
+
+	//"database/sql"
 
 	"github.com/hriday111/go-rest-api/internal/db"
+	"github.com/hriday111/go-rest-api/internal/models"
+	"github.com/hriday111/go-rest-api/internal/utils"
 )
 
-
-type User struct {
-	Name  string `json:"name"`
-	Email string `json:"email"`
-}
-func main(){
+func main() {
 
 	db.Connect()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -24,7 +23,7 @@ func main(){
 	http.HandleFunc("/register", registerHandler)
 
 	fmt.Println("Starting server on :8080...")
-	err:= http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8081", nil)
 	if err != nil {
 		log.Fatalf("Error starting server: %v", err)
 	}
@@ -32,21 +31,36 @@ func main(){
 }
 
 func registerHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost{
+	if r.Method != http.MethodPost {
 		http.Error(w, "Only POST Methodallowed", http.StatusMethodNotAllowed)
 		return
 	}
 
-	var user User
+	var user models.User
 
 	err := json.NewDecoder(r.Body).Decode(&user)
-	if err != nil{
+	if err != nil {
 		http.Error(w, "Invalid request payload", http.StatusBadRequest)
 		return
 	}
 
+	// Hash the password
+	hashedPassword, err := utils.HashPassword(user.Password)
+	if err != nil {
+		http.Error(w, "Error hashing password", http.StatusInternalServerError)
+		return
+	}
+	user.Password = hashedPassword
+	// Save the user to DB
+
+	query := `INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING id`
+	err = db.DB.QueryRow(query, user.Name, user.Email, user.Password).Scan(&user.ID)
+	if err != nil {
+		http.Error(w, "Error inserting user: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
 	fmt.Printf("Recieved registration: Name=%s, Email=%s\n", user.Name, user.Email)
 
 	w.WriteHeader(http.StatusCreated)
-	fmt.Fprintln(w, "User registered successfully!")
+	fmt.Fprintln(w, "User registered successfully! ID:", user.ID)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/hriday111/go-rest-api
 
 go 1.24.2
 
-require github.com/lib/pq v1.10.9 // indirect
+require (
+	github.com/lib/pq v1.10.9 // indirect
+	golang.org/x/crypto v0.37.0 // indirect
+)
